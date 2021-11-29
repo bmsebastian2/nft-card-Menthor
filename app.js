94487856481f9eccff835e021787102a9022bf26
sebastianbm <bmsebastian2@gmail.com>
2021-11-29 12:42:35 -0300

    Configuracion responsive table

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const imagen = document.querySelector('.imgQua')
 imagen.addEventListener('mouseenter',()=>{
      imagen.src = './images/image-equilibrium-2.gif';    
 })
+
 imagen.addEventListener('mouseleave',()=>{
     imagen.src = '/images/image-equilibrium.jpg';
    
