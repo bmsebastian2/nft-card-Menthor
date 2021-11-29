dfae07a091373647481b0270db680ac678b1d85d
sebastianbm <bmsebastian2@gmail.com>
2021-11-29 15:20:55 -0300

    Se coloco elmouseover y el mouseout dentro del mismo html y haciendo el this.

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 
-const imagen = document.querySelector('.imgQua')
+// window.addEventListener('load', inciciar, false)
 
-imagen.addEventListener('mouseenter',()=>{
-     imagen.src = './images/image-equilibrium-2.gif';    
-})
 
-imagen.addEventListener('mouseout',()=>{
-     imagen.src = '/images/image-equilibrium.jpg';   
-})
+// function iniciar () {
+
+// }
+
+// const imagen = document.querySelector('.imgQua')
+
+// imagen.addEventListener('mouseenter',()=>{
+//      imagen.src = './images/image-equilibrium-2.gif';    
+// })
+
+// imagen.addEventListener('mouseout',()=>{
+//     //  imagen.src = '/images/image-equilibrium.jpg';   
+//     imagen.style.border= '1px solid red'
+// })
 
